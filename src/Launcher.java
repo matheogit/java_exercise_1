c1be5b062f5c155ca05c06d516d839a29fe28392
Louerat Matheo <louerat@et.esiea.fr>
2021-11-29 15:55:09 +0100

    launcher boucle et fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,9 +4,31 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.print("Bienvenue\n");
         Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine();
+        while(!tmp.equals("quit")){
+            if (tmp.equals("fibo")){
+                System.out.print("Entrer une valeur:\n");
+                int n = scanner.nextInt();
+                int f0=0,f1=1,f=0;
+                for (int i = 1; i < n; i++){
+                    f = f0 + f1;
+                    f0 = f1;
+                    f1 = f;
+                }
+                if(n == 1){
+                    System.out.print("1\n");
+                }
+                else{
+                    System.out.print(f);
+                    System.out.print("\n");
+                }
+                scanner.nextLine();
+                tmp = scanner.nextLine();
 
-        while(!scanner.nextLine().equals("quit")){
-            System.out.print("Unknown command\n");
+            }else{
+                System.out.print("Unknown command\n");
+                tmp = scanner.nextLine();
+            }
         }
     }
 }
