3525ead24bbce14dd2c892530d07f026143464f5
Louerat Matheo <louerat@et.esiea.fr>
2021-11-30 08:56:43 +0100

    launcher freq

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Launcher {
     public static void main(String[] args) {
@@ -25,7 +29,35 @@ public class Launcher {
                 scanner.nextLine();
                 tmp = scanner.nextLine();
 
-            }else{
+            } else if(tmp.equals("freq")){
+                System.out.print("Entrer un chemin de fichier\n");
+                String path = scanner.nextLine();
+                try {
+                    String content = java.nio.file.Files.readString(Paths.get(path));
+                    content = content.toLowerCase();
+                    String[] words = content.split("\\W+");
+                    Map<String, Long> map = Arrays.stream(words)
+                            .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+                    Map<String, Long> result = map.entrySet()
+                            .stream()
+                            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                            .limit(3)
+                            .collect(Collectors.toMap(
+                                    Map.Entry::getKey,
+                                    Map.Entry::getValue,
+                                    (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+                    for (Map.Entry<String, Long> entry : result.entrySet()) {
+                        System.out.print(entry.getKey() + " ");
+                    }
+                    System.out.print("\n");
+
+                } catch(IOException e){
+                    System.out.print("Unreadable file IOException: "+ e + "\n");
+                }
+                tmp = scanner.nextLine();
+
+            } else{
                 System.out.print("Unknown command\n");
                 tmp = scanner.nextLine();
             }
